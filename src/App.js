72975d6ee7acc0ebeb7b93254cd81ac2665ad957
Ezequiel124 <94939206+Ezequiel124@users.noreply.github.com>
2023-12-07 11:21:02 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Proyecto final de Azure</h1>
+        <img src={logo} className="App-logo" alt="logo" />        
         <p> 
+          Proyecto final de Azure
           Integrantes del equipo:
             <br>1. Mauricio Ezequiel Puente Rocha
             <br>2. Omar Segura Sena
